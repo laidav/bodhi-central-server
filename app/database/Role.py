1bbf3f14f2548e7e913e29e2ad9a6844d4d2e845
DavidLai <david.lai@postbeyond.com>
2019-02-24 18:19:18 -0500

    set up role and user tables

--- app/database/Role.py
+++ app/database/Role.py
@@ -6,5 +6,7 @@ class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
 
+    users = db.relationship('User', backref='role')
+
     def __repr__(self):
         return '<Role %r>' % self.name
--- app/database/User.py
+++ app/database/User.py
@@ -6,5 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
 
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
     def __repr__(self):
         return '<User %r>' % self.username
