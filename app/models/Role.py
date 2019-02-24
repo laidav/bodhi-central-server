b02c8aeff89e9bc462ffc67ce15e477548e47205
DavidLai <david.lai@postbeyond.com>
2019-02-24 17:25:11 -0500

    add User and Role classes

---
+++ app/models/Role.py
@@ -0,0 +1,10 @@
+from .. import db
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
