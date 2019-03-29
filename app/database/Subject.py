e5687c239278296651d46c4c855252644de3ee62
DavidLai <david.lai@postbeyond.com>
2019-03-29 06:59:15 -0400

    updated subject models and seed data

--- app/database/Subject.py
+++ app/database/Subject.py
@@ -5,10 +5,19 @@ from .PostSubject import PostSubject
 class Subject(db.Model):
     __tablename__ = "subjects"
     id = db.Column(db.Integer, primary_key=True)
-    parent_id = db.Column(db.Integer, db.ForeignKey("subjects.id"))
+    first_child_id = db.Column(db.Integer, db.ForeignKey("subjects.id"))
+    right_sibling_id = db.Column(db.Integer, db.ForeignKey("subjects.id"))
     name = db.Column(db.String(64), unique=True)
 
-    parent = db.relationship("Subject", backref="children", remote_side=[id])
+    first_child = db.relationship("Subject", backref="parent",
+                                  remote_side=[id],
+                                  uselist=False,
+                                  foreign_keys=first_child_id)
+    right_sibling = db.relationship("Subject", backref="left_sibling",
+                                    remote_side=[id],
+                                    uselist=False,
+                                    foreign_keys=right_sibling_id)
+
     post_subjects = db.relationship("PostSubject",
                                     foreign_keys=[PostSubject.post_id, PostSubject.subject_id],
                                     backref=db.backref("subject", lazy="joined"),
@@ -21,6 +30,7 @@ class Subject(db.Model):
     def to_json(self):
         return {
             "id": self.id,
-            "parent_id": self.parent_id,
+            "first_child_id": self.first_child_id,
+            "right_sibling_id": self.right_sibling_id,
             "name": self.name
         }
--- change_scripts/2019-03-03_initial_seed_data.py
+++ change_scripts/2019-03-03_initial_seed_data.py
@@ -25,18 +25,53 @@ if __name__ == "__main__":
                           email="laidav@gmail.com",
                           confirmed=True)
         user_david.password = "Password1!"
+
+        buddha = Subject(name="Buddha")
         wisdom = Subject(name="Wisdom")
         ethics = Subject(name="Ethics")
         meditation = Subject(name="Meditation")
-        right_view = Subject(name="Right View", parent=wisdom)
-        right_intention = Subject(name="Right Intention", parent=wisdom)
+
+        buddha.first_child = wisdom
+        wisdom.right_sibling = ethics
+        ethics.right_sibling = meditation
+
+        right_view = Subject(name="Right View")
+        right_intention = Subject(name="Right Intention")
+
+        wisdom.first_child = right_view
+        right_view.right_sibling = right_intention
+
+        right_speech = Subject(name="Right Speech")
+        right_action = Subject(name="Right Action")
+        right_livelihood = Subject(name="Right Livelihood")
+
+        ethics.first_child = right_speech
+        right_speech.right_sibling = right_action
+        right_action.right_sibling = right_livelihood
+
+        right_effort = Subject(name="Right Effort")
+        right_mindfulness = Subject(name="Right Mindfulness")
+        right_concentration = Subject(name="Right Concentration")
+
+        meditation.first_child = right_effort
+        right_effort.right_sibling = right_mindfulness
+        right_mindfulness.right_sibling = right_concentration
+
         post = Post(title="Ajahn Chah: The Peace Beyond",
                     description="Ajahn Chah gives talk on the birth and death of suffering",
                     link="https://www.youtube.com/watch?v=Mtqz82w4v0s",
                     author=user_david)
 
+        post_2 = Post(title="Ajahn Chah: No Abiding",
+                      description="Ajahn Chah gives talk on permenance and the beauty of being empty",
+                      link="https://www.youtube.com/watch?v=b5vltJm9UrM&t=116s",
+                      author=user_david)
+
         post_subject_a = PostSubject(post=post, subject=right_view)
         post_subject_b = PostSubject(post=post, subject=right_intention)
+
+        post_2_subject_a = PostSubject(post=post_2, subject=right_view)
+        post_2_subject_b = PostSubject(post=post_2, subject=right_intention)
 
         practice = Practice(teaching_point="Happiness and Unhappiness are "
                                            "equal in disturbing peace and enlightment."
@@ -54,11 +89,13 @@ if __name__ == "__main__":
                                           "on' to 'letting go'.  The breath is so beautiful, maybe life is"
                                           "all about holding and letting go, just like the breath",
                               author=user_david,
-                              post=post)
+                              post=post_2)
 
-        db.session.add_all([admin_role, mod_role, user_role, user_david, wisdom,
-                            ethics, meditation, right_view, right_intention, post,
-                            post_subject_a, post_subject_b, practice, practice_2])
+        db.session.add_all([admin_role, mod_role, user_role, user_david, buddha, wisdom,
+                            ethics, meditation, right_view, right_intention, right_speech,
+                            right_action, right_livelihood, right_effort, right_mindfulness,
+                            right_concentration, post, post_2, post_subject_a, post_subject_b, practice,
+                            practice_2])
 
         db.session.commit()
 
