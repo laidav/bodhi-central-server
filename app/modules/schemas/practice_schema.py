46acb83e7b1d7e444ae0251f9f3ef9b2924b43f3
DavidLai <david.lai@postbeyond.com>
2019-04-01 19:40:02 -0400

    updated schema to ensure list of subjects are greater than zero and are integers

--- app/modules/schemas/practice_schema.py
+++ app/modules/schemas/practice_schema.py
@@ -1,8 +1,10 @@
-from schema import Schema, Or, Optional, Use
+from schema import Schema, Or, Optional, Use, And
 
 BASE_PRACTICE_SCHEMA = {
     "teaching_point": Or(str, error="Invalid teaching_point"),
-    "subjects": Or(list, error="Invalid Subjects.  Should be an array of integers"),
+    "subjects": And(list,
+                    lambda n: all(isinstance(item, int) and item > 0 for item in n),
+                    error="Invalid Subjects.  Should be an array of integers"),
     Optional("application"): Or(str, error="Invalid application should be a string type"),
     Optional("post_id"): Use(int, error="Invalid Post Id.  Must be an integer.")
 }
