781ed1c376cbd1df0007a61483ca230a3260f05b
DavidLai <david.lai@postbeyond.com>
2019-04-04 06:07:18 -0400

    added check in practice schema so subject is never empty

--- app/modules/schemas/practice_schema.py
+++ app/modules/schemas/practice_schema.py
@@ -1,9 +1,9 @@
 from schema import Schema, Or, Optional, Use, And
 
 BASE_PRACTICE_SCHEMA = {
-    "teaching_point": Or(str, error="Invalid teaching_point"),
+    "teaching_point": And(str, lambda n: n, error="Invalid teaching_point"),
     "subjects": And(list,
-                    lambda n: all(isinstance(item, int) and item > 0 for item in n),
+                    lambda n: all(isinstance(item, int) and item > 0 for item in n) and n,
                     error="Invalid Subjects.  Should be an array of integers"),
     Optional("application"): Or(str, error="Invalid application should be a string type"),
     Optional("post_id"): Use(int, error="Invalid Post Id.  Must be an integer.")
