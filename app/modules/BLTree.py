e655d499523f34b30b932a25f8684feddd8ff603
DavidLai <david.lai@postbeyond.com>
2019-04-08 19:51:27 -0400

    added descendant searcher

--- app/modules/BLTree.py
+++ app/modules/BLTree.py
@@ -2,16 +2,16 @@ class BLTree:
     def __init__(self, root_node):
         self.root_node = root_node
 
-    def dfs_traversal(self):
-
+    def dfs_traversal(self, searcher):
         def __depth_traversal_recursive(node):
             nonlocal current_node
             current_node = node.first_child
             __check_siblings_recursive(current_node)
 
         def __check_siblings_recursive(node):
-            if node is not None:
-                print(node.name, node.id)
+            searcher.compare(node)
+            if searcher.search_done:
+                return searcher
 
             if node is None:
                 return
@@ -25,8 +25,24 @@ class BLTree:
 
         current_node = self.root_node
 
-        print(current_node.name, current_node.id)
+        searcher.compare(current_node)
 
         __depth_traversal_recursive(current_node)
 
-        return
+        searcher.search_done = True
+        return searcher
+
+
+class BLTreeSearcher:
+    def __init__(self, results):
+        self.results = results
+        self.search_done = False
+
+
+class DescendantSearcher(BLTreeSearcher):
+    def __init__(self):
+        BLTreeSearcher.__init__(self, [])
+
+    def compare(self, node):
+        if node is not None:
+            self.results.append(node)
