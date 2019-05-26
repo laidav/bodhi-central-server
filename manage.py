4619699d82eaedc6749f344657872c8bdbb584cf
Dave <laidav@gmail.com>
2019-05-26 07:07:51 -0400

    create deploy method in manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from app import create_app, db
 from app.db_models import User, Role, Post, Subject, PostSubject, Practice, PracticeSubject
 from app.modules.BCTree import BCTree
 from flask_script import Manager, Shell
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate, MigrateCommand, upgrade
 
 app = create_app(os.getenv("FLASK_CONFIG") or "default")
 manager = Manager(app)
@@ -15,6 +15,11 @@ def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role, Post=Post,
                 Subject=Subject, PostSubject=PostSubject, Practice=Practice,
                 PracticeSubject=PracticeSubject, BCTree=BCTree)
+
+
+@manager.command
+def deploy():
+    upgrade()
 
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
