6857e8e53dbf5f9109b55a6f1313d0797c6004e6
Dave <laidav@gmail.com>
2019-05-26 08:38:49 -0400

    added ssl to config and init

--- config.py
+++ config.py
@@ -14,13 +14,16 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URL") or "sqlite:///" + os.path.join(basedir,
                                                                                                 "data-dev.sqlite")
+    SSL_DISABLE = True
 
 
 class ProductionConfig(Config):
+    SSL_DISABLE = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 
 class HerokuConfig(ProductionConfig):
+
     @classmethod
     def init_app(cls, app):
         ProductionConfig.init_app(app)
