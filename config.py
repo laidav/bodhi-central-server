bd189b1923a7cdb1e1b8846c91191d002a23d9f2
Dave <laidav@gmail.com>
2019-06-01 08:03:56 -0400

    add extra variables to config for mail

--- config.py
+++ config.py
@@ -3,7 +3,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "fesser lippen chip chalkwithering licktackle fef foundation"
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY") or "fesser lippen chip chalkwithering licktackle fef foundation"
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+        ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SSL_REDIRECT = False
 
     @staticmethod
@@ -19,6 +27,7 @@ class DevelopmentConfig(Config):
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
 
 class HerokuConfig(ProductionConfig):
     SSL_REDIRECT = True if os.environ.get("DYNO") else False
