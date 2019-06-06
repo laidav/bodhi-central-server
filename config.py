ba5139cc3e3f5d49f56dffeba901ecbcd7a1c87a
Dave <laidav@gmail.com>
2019-06-06 05:18:23 -0400

    updated production config to log errors to prod email

--- config.py
+++ config.py
@@ -12,6 +12,9 @@ class Config:
         ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    BODHICENTRAL_MAIL_SUBJECT_PREFIX = '[BodhiCentral]'
+    BODHICENTRAL_MAIL_SENDER = 'BodhiCentral Admin <laidavtest@gmail.com>'
+    BODHICENTRAL_ADMIN = os.environ.get('BODHICENTRAL_ADMIN')
     SSL_REDIRECT = False
 
     @staticmethod
@@ -27,6 +30,29 @@ class DevelopmentConfig(Config):
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        # email errors to the administrators
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_TLS', None):
+                secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.BODHICENTRAL_MAIL_SENDER,
+            toaddrs=[cls.BODHICENTRAL_ADMIN],
+            subject=cls.BODHICENTRAL_MAIL_SUBJECT_PREFIX + ' Application Error',
+            credentials=credentials,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
 
 class HerokuConfig(ProductionConfig):
