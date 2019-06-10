6f128f40423de971b4d6f47797c4ecf82ecf08ee
Dave <laidav@gmail.com>
2019-06-10 19:01:25 -0400

    updated practice schema to limit length of teaching_point

--- app/modules/schemas/practice_schema.py
+++ app/modules/schemas/practice_schema.py
@@ -1,9 +1,10 @@
 from schema import Schema, Or, Optional, Use, And
 
 BASE_PRACTICE_SCHEMA = {
-    "teaching_point": And(str, lambda n: n, error="Invalid teaching_point"),
+    "teaching_point": And(str, len, lambda n: len(n) < 250, error="Invalid teaching_point"),
     "subjects": And(list,
-                    lambda n: all(isinstance(item, int) and item > 0 for item in n) and n,
+                    lambda n: all(isinstance(item, int)
+                                  and item > 0 for item in n) and n,
                     error="Invalid Subjects.  Should be an array of integers"),
     Optional("application"): Or(str, error="Invalid application should be a string type"),
     Optional("post_id"): Use(int, error="Invalid Post Id.  Must be an integer.")
