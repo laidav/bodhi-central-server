54b284281ab6d4a81d72daa840ae771ca06cdc33
David Lai <laidav@gmail.com>
2019-07-11 12:31:47 -0400

    added email regex for valid email validation

--- app/modules/schemas/auth_schema.py
+++ app/modules/schemas/auth_schema.py
@@ -5,8 +5,12 @@ ADD_USER_SCHEMA = {
     "username": And(str,
                     len,
                     lambda n: re.search("^[A-Za-z][A-Za-z0-9_.]*$", n),
-                    error="Must be an non empty string"),
-    "email": And(str, len, error="Email is required"),
+                    error="Must be an non empty string, and contain only letters, numbers, underscores and dots"),
+    "email": And(str,
+                 len,
+                 lambda n: re.search(
+                     "(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", n),
+                 error="Email is required and be of valid format"),
     "password": And(str, len, error="Password is required"),
     "password2": And(str, len, error="Confirm password is required"),
 }
